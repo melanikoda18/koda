67b8b382673203479ea50906c1557d62d1b57b6a
melanikoda18 <vmelani193@gmail.com>
2023-09-07 18:10:21 -0400

    me

--- practica.py
+++ practica.py
@@ -1,16 +1,16 @@
-nombre=input("ingrese su nombre ").title()
-apellido=input("ingrese su apellido  ").upper()
+nombre=input("ingrese su nombre ").title()# el title sirve para poner la primera letra de cada palabra en mayuscula 
+apellido=input("ingrese su apellido  ").upper() # el upper sirve para poner todas la frace en mayuscula 
 cargo=input("imgrese su cargo en la empresa  ").title()
-correo=input("ingrese su correo ").lower()
+correo=input("ingrese su correo ").lower() #el lower sirve para poner toda la oracion  en  minuscula
 ID=input(" ingrese su cedula ")
 edad=input("edad ")
-print("--------")
+print("---------------------------------------")
+
 print(f"{apellido}, {nombre}")
 print(f"{cargo}, {edad}")
- 
-
-
 print(correo)
 print()
-print(ID+"-"+ str (len(ID)))
-print("--------------")
+print(ID+"-"+ str (len(ID))) # el str sirve para tranformar una variable o un numero  en "texto" y el len para indicar el largo de algo 
+print("-----------------------------------------------")
+
+print(f "  str (len(apellido)) )
